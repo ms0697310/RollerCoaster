65994b61ff88913e300f535fe87c75cce48391a9
CCCpeggy <peggy19925@gmail.com>
2020-01-14 04:10:23 +0800

    feat(Stone): add stone

--- Roller Coaster/Include/Stone.h
+++ Roller Coaster/Include/Stone.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "Shader.h"
-class Hill :
-	public Shader
+#include <vector>
+class Stone : public Shader
 {
 private:
 	QVector<QVector4D> vertices;
 	QVector<QVector3D> normal;
 	QOpenGLBuffer vertex_vbo;
 	QOpenGLBuffer normal_vbo;
+	float x, z;
 public:
-	Hill();
+	Stone(float x = 0, float z = 0);
 	void PaintObject();
-	static float ComputePoint(float xx, float zz);
 };
 
